55874a025af3f75826fecb1304daef6e350b3728
khromov <stanislav.khromov@gmail.com>
2015-03-01 17:10:40 +0100

    API class WIP

--- src/WordPressOrgAPI.class.php
+++ src/WordPressOrgAPI.class.php
@@ -10,4 +10,40 @@ class WordPressOrgAPI
     function __construct()
     {
     }
+
+    /**
+     * Secret Key
+     *
+     * Secret key generator for wp-config.php.
+     * @param string $version
+     * @param bool $salt
+     */
+    function secret_key($version = '1.1', $salt = true)
+    {
+
+    }
+
+    /**
+     * Stats
+     */
+
+    function stats_wordpress($version = '1.0')
+    {
+    }
+
+    function stats_php($version = '1.0')
+    {
+    }
+
+    function stats_mysql($version = '1.0')
+    {
+    }
+
+    function stats_plugin($slug, $version = '1.0')
+    {
+    }
+
+    function stats_plugin_downloads($slug, $limit, $callback, $version = '1.0')
+    {
+    }
 }
